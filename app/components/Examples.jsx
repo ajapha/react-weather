b8664cb1fc1a3b06b7290490115057a7ff52bbcd
Aaron Japha <aaronjapha@gmail.com>
2019-12-22 00:05:57 -0500

    add examples and grid system

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,8 +1,20 @@
 const React = require('react');
+const { Link } = require('react-router');
 
 module.exports = () => (
     <div>
-        <h3>Examples</h3>
-        <p>Welcome to the examples page</p>
+        <h1 className="text-center">Examples</h1>
+        <p>Here are a few example locations to try out</p>
+        <ol>
+            <li>
+                <Link to="/?location=Philadelphia">Philadelphia, PA</Link>
+            </li>
+            <li>
+                <Link to="/?location=Rio">Rio, Brazil</Link>
+            </li>
+            <li>
+                <Link to="/?location=Arlington">Arlington, VA</Link>
+            </li>
+        </ol>
     </div>
 );
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -4,7 +4,10 @@ const Nav = require('Nav');
 module.exports = props => (
     <div>
         <Nav />
-        <h2>Main Component</h2>
-        {props.children}
+        <div className="row">
+            <div className="columns medium-6 large-4 small-centered">
+                {props.children}
+            </div>
+        </div>
     </div>
 );
