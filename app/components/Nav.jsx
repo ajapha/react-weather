e9638e3a5de9bab65111fcf0dc6ec5ec486082ac
Aaron Japha <aaronjapha@gmail.com>
2019-12-21 23:54:39 -0500

    redo nav bar for styles and search

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,11 +1,41 @@
 const React = require('react');
 const { IndexLink, Link } = require('react-router');
 
-module.exports = () => (
-    <div>
-        <h2>Nav Component</h2>
-        <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-        <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-        <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-    </div>
-);
+module.exports = React.createClass({
+    onSearch: function (e) {
+        e.preventDefault();
+        alert('Not ready yet');
+    },
+    render: function() {
+        return (
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="menu">
+                        <li className="menu-text">React Weather App</li>
+                        <li>
+                            <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+                        </li>
+                        <li>
+                            <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+                        </li>
+                        <li>
+                            <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="top-bar-right">
+                    <form onSubmit={this.onSearch}>
+                        <ul className="menu">
+                            <li>
+                                <input type="search" placeholder="search weather" />
+                            </li>
+                            <li>
+                                <input type="button" className="button" value="Get Weather" />
+                            </li>
+                        </ul>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+});
