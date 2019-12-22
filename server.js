9f5cfdd215f24b9cff9930a71a97c7345532ecc6
Aaron Japha <aaronjapha@gmail.com>
2019-12-21 23:22:14 -0500

    changes to prepare for heroku

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ const express =  require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    if (req.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect(`http://${req.hostname}${req.url}`);
+    }
+});
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-    console.log('Express listening on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Express listening on port ${port}`);
 });
